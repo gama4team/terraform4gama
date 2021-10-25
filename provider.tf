96c1c73a5dfcca536fd8c2c398be42d3c62024bf
gama4team <gama4team@gmail.com>
2021-10-25 16:02:52 -0300

    Retirando versao terraform

--- provider.tf
+++ provider.tf
@@ -1,20 +1,16 @@
 provider "aws" {
-  version = "~> 2.50"
   region = "region"
 }
 
-terraform {
-  backend "s3" {
-    bucket                      = "gamateam4"
-    key                         = "gamateam4/terraform.tfstate"
-    region                      = "sa-east-1"
-    encrypt                     = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket                      = "gamateam4"
+#    key                         = "gamateam4/terraform.tfstate"
+#    region                      = "sa-east-1"
+#    encrypt                     = true
+#    skip_credentials_validation = true
+#    skip_metadata_api_check     = true
+#  }
+#}
 
-provider "external" {
-  version = "~> 1.2"
-}
 
